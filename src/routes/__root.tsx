c4de8d968e9e699790b2dfb8dc5dc490ad4ad847
claudiojas <claudiojasoares.dev@gmail.com>
2025-09-24 22:58:37 -0300

    fix: fixing layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,19 +3,21 @@ import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-4 flex gap-4 bg-gray-800 text-white">
-        <Link to="/" className="[&.active]:font-bold hover:underline">
-          Caixa
-        </Link>
-        <Link to="/products/add" className="[&.active]:font-bold hover:underline">
-          Adicionar Produto
-        </Link>
-        <Link to="/wristbands" className="[&.active]:font-bold hover:underline">
-          Criar Pulseira
-        </Link>
-        <Link to="/wristbands-overview" className="[&.active]:font-bold hover:underline">
-          Consultar Pulseiras
-        </Link>
+      <div className='py-5 px-8 flex items-center justify-start text-white w-full bg-gray-900'>
+          <div className="w-[30%] flex items-center justify-between">
+            <Link to="/" className="[&.active]:font-bold hover:underline">
+              Caixa
+            </Link>
+            <Link to="/products/add" className="[&.active]:font-bold hover:underline">
+              Adicionar Produto
+            </Link>
+            <Link to="/wristbands" className="[&.active]:font-bold hover:underline">
+              Criar Pulseira
+            </Link>
+            <Link to="/wristbands-overview" className="[&.active]:font-bold hover:underline">
+              Consultar Pulseiras
+            </Link>
+        </div>
       </div>
       <hr className="border-gray-700" />
       <Outlet />
